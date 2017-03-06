83b9f842327bce4456d5e8d5a910361da338b09d
Andy <Andy@Admins-MacBook-Air.local>
2017-03-05 22:28:35 -0500

    commit

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -6,7 +6,7 @@ var clues = [ "The native range of this fish includes the Lake Ontario and Lake 
  "Fossil records of this fish date back over 300 million years, nearly 50 million years before dinosaurs first appeared.",
  "This is a very popular sport fish, as it is easy to catch during their feeding times. It is also very tasty, despite its name.",
  "This is a species of cyprinid fish endemic to the Chattahoochee River drainage in the states of Georgia and Alabama. The average length is about 7 centimeters, or 2 inches.",
- "Commonly named swampfish, ricefish, or riceditch killifish, it is a freshwater fish of the family Amblyopsidae. It is the only living species of the genus Chologaster.",
+ "Commonly named swampfish, ricefish, or riceditch killifish, it is a freshwater fish of the family Amblyopsidae. It is the only living species of its titular genus.",
  "This is a species of cartilaginous fish. It is easily identified by the translucent patches on either side of its snout",
  "This is the world's longest bony fish. Its physical characteristics and its undulating mode of swimming have led to speculation that it might be the source of many 'sea serpent' sightings."]
 //create variable for wins, losses, lives, and incorrect letters to be displayed
@@ -150,13 +150,14 @@ document.onkeyup = function(event){
 	document.getElementById("Guesses").innerHTML = incorrectGuess.join(" ");
 
 	if ( lives < 1) {
-
-		alert("Sorry, the answer was " + wordInUse.toUpperCase());
+		
+		alert("Sorry, the answer is the " + wordInUse.toUpperCase());
 		gameLost();
 	}
 
 	if ( blanksAndSuccesses.indexOf("_") == "-1"){
-		alert("You got it! The answer is the " + wordInUse);
+		document.getElementById("Word").innerHTML = blanksAndSuccesses.join(" ");
+		alert("You got it! The answer is the " + wordInUse.toUpperCase());
 		gameWon();
 	}
 }
